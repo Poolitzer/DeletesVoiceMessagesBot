6ba2bcfd1561b02c28b1124862e6184abf075cee
Poolitzer <deadpool10@web.de>
2018-09-09 13:30:34 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,205 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+from telegram.utils.helpers import mention_markdown
+from telegram.error import BadRequest, Unauthorized
+
+import json
+import logging
+import languages
+from time import sleep
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+data_file = open('./database.json')
+database = json.load(data_file)
+
+updater = Updater(token='686965201:AAE4CfqQmwkH5kOeO6ri7w_mcCd_aQFyDpY')
+dispatcher = updater.dispatcher
+
+
+def start(bot, update, args):
+    language = save(update)
+    if not args:
+        bot.send_message(chat_id=update.message.chat_id, text=languages.start["{}".format(language)])
+    else:
+        real_id = args[0][5:len(args[0])]
+        keyboard = [[InlineKeyboardButton(languages.english["{}".format(language)],
+                                          callback_data='en{}'.format(real_id)),
+                     InlineKeyboardButton(languages.german["{}".format(language)],
+                                          callback_data='de{}'.format(real_id))],
+                    ]
+        languages_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text(text=languages.setting["{}".format(language)], reply_markup=languages_markup)
+
+
+def helps(bot, update):
+    language = save(update)
+    if update.effective_chat.type == "private":
+        bot.send_message(chat_id=update.message.chat_id, text=languages.helps["{}".format(language)])
+
+
+def lang(bot, update):
+    language = save(update)
+    keyboard = [[InlineKeyboardButton(languages.english["{}".format(language)],
+                                      callback_data='en{}'.format(update.message.chat_id)),
+                 InlineKeyboardButton(languages.german["{}".format(language)],
+                                      callback_data='de{}'.format(update.message.chat_id))],
+                ]
+    starting = [[InlineKeyboardButton("Start",
+                                      "https://t.me/DeleteVoiceMessagesBot?start=start{}".format(update.message.chat_id)
+                                      )]
+                ]
+    languages_markup = InlineKeyboardMarkup(keyboard)
+    start_markup = InlineKeyboardMarkup(starting)
+    found = False
+    if update.effective_chat.type != "private":
+        admins = bot.getChatAdministrators(update.message.chat_id)
+        for ids in admins:
+            if ids['user']["id"] == update.message.from_user.id:
+                found = True
+                try:
+                    bot.send_message(chat_id=update.message.from_user.id, text=languages.setting["{}".format(language)],
+                                     reply_markup=languages_markup)
+                except Unauthorized:
+                    update.message.reply_text(text=languages.starts["{}".format(language)], reply_markup=start_markup)
+        if not found:
+            update.message.reply_text(languages.admin["{}".format(language)])
+    else:
+        update.message.reply_text(text=languages.setting["{}".format(language)], reply_markup=languages_markup,
+                                  parse_mode='MARKDOWN')
+
+
+def buttons(bot, update):
+    language = save(update)
+    query = update.callback_query
+    real_id = query.data[2:len(query.data)]
+    if real_id.startswith("-100"):
+        for ids in database["groups"]:
+            if ids[0] == int(real_id):
+                ids[1] = query.data[0:2]
+                with open('./database.json', 'w') as outfile:
+                    json.dump(database, outfile, indent=4, sort_keys=True)
+                break
+    else:
+        for ids in database["users"]:
+            if ids[0] == int(real_id):
+                ids[1] = query.data[0:2]
+                with open('./database.json', 'w') as outfile:
+                    json.dump(database, outfile, indent=4, sort_keys=True)
+                break
+    bot.edit_message_text(text=languages.selected["{}".format(language)].format(query.data[0:2]),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id, parse_mode='MARKDOWN')
+
+
+def voice(bot, update):
+    x = 5
+    language = save(update)
+    try:
+        update.message.delete()
+        user = mention_markdown(update.message.from_user.id, update.message.from_user.first_name)
+        message = bot.send_message(chat_id=update.message.chat_id,
+                                   text=languages.delete["{}".format(language)].format(user, x), parse_mode='markdown')
+        while True:
+            x -= 1
+            message.edit_text(text=languages.delete["{}".format(language)].format(user, x), parse_mode='markdown')
+            if x is 0:
+                message.delete()
+                stats("groups", update.message.chat_id)
+                break
+            sleep(1)
+    except BadRequest:
+        if update.effective_chat.type == "private":
+            update.message.reply_text(languages.private["{}".format(language)])
+            stats("users", update.message.chat_id)
+        else:
+            update.message.reply_text(languages.fail["{}".format(language)])
+
+
+def commands(bot, update):
+    language = save(update)
+    if update.effective_chat.type == "private":
+        update.message.reply_text(languages.commands_p["{}".format(language)])
+    else:
+        update.message.reply_text(languages.commands_g["{}".format(language)])
+
+
+def statistic(bot, update):
+    language = save(update)
+    group = 0
+    if update.effective_chat.type == "private":
+        for ids in database["users"]:
+            if ids[0] == update.message.chat_id:
+                update.message.reply_text(languages.private_stats["{}".format(language)].format(ids[2]))
+                break
+    else:
+        user = mention_markdown(update.message.from_user.id, update.message.from_user.first_name)
+        for ids in database["groups"]:
+            if ids[0] == update.message.chat_id:
+                group = ids[2]
+                break
+        for chats in database["users"]:
+            if chats[0] == update.message.from_user.id:
+                update.message.reply_text(languages.group_stats["{}".format(language)].format(group, user, chats[2]),
+                                          parse_mode="markdown")
+                break
+
+
+def add(bot, update):
+    language = save(update)
+    for ids in update.message.new_chat_members:
+        if ids["id"] is 659659397:
+            bot.send_message(chat_id=update.message.chat_id, text=languages.joined["{}".format(language)])
+            break
+
+
+def stats(chat, real_id):
+    for ids in database[chat]:
+        if ids[0] == real_id:
+            ids[2] += 1
+            break
+    with open('./database.json', 'w') as outfile:
+        json.dump(database, outfile, indent=4, sort_keys=True)
+
+
+def save(update):
+    skip = False
+    try:
+        if update.effective_chat.type == "private":
+            chat = "users"
+            real_id = update.message.chat_id
+        else:
+            chat = "groups"
+            real_id = update.message.chat_id
+    except AttributeError:
+        chat = "users"
+        real_id = update.callback_query.from_user.id
+    for ids in database[chat]:
+        if ids[0] == real_id:
+            skip = True
+            language = ids[1]
+            return language
+    if not skip:
+        database[chat].append([real_id, "en", 0])
+        with open('./database.json', 'w') as outfile:
+            json.dump(database, outfile, indent=4, sort_keys=True)
+            language = "en"
+            return language
+
+
+start_handler = CommandHandler('start', start, pass_args=True)
+dispatcher.add_handler(start_handler)
+lang_handler = CommandHandler('lang', lang)
+dispatcher.add_handler(lang_handler)
+helps_handler = CommandHandler('help', helps)
+dispatcher.add_handler(helps_handler)
+commands_handler = CommandHandler('commands', commands)
+dispatcher.add_handler(commands_handler)
+statistic_handler = CommandHandler('stats', statistic)
+dispatcher.add_handler(statistic_handler)
+language_handler = CallbackQueryHandler(buttons)
+dispatcher.add_handler(language_handler)
+voice_handler = MessageHandler(Filters.voice, voice)
+dispatcher.add_handler(voice_handler)
+added_handler = MessageHandler(Filters.status_update.new_chat_members, add)
+dispatcher.add_handler(added_handler)
+updater.start_polling()
---
+++ languages.py
@@ -0,0 +1,95 @@
+start = {"en": "Hello. If my name didn't already gave it away: My only purpose is to delete voice messages in groups."
+               "That's what I do and I'm pretty good at it. Run /help if you need additional help or /lang if you want "
+               "to change the language.",
+         "de": "Hallo. Du wolltest dir also die Startnachricht mal in Deutsch angucken? Interessant. Aber ich habe "
+               "dran gedacht :). Denk dran, /help zu schreiben, wenn du Hilfe brauchst - oder /lang, um die Sprache "
+               "zu ändern."
+         }
+
+helps = {"en": "The way I work is very simple. You add me as an admin to your group I'm going to delete every "
+               "voicemessage from then on and will tell your fellow members that your group don't want any voice "
+               "messages in it. Send /commands for my commands (not that much :). If you want to see the source code "
+               "behind me, have a look at my repo: https://github.com/Poolitzer/DeletesVoiceMessagesBot. If you have "
+               "any questions, suggestions or want to add a language to the bot, please ping my master: @Poolitzer. "
+               "He speaks german and english.",
+         "de": "Ich bin ein sehr einfacher Bot: Du fügst mich einer Gruppe als Admin hinzu, ich lösche alle "
+               "Sprachnachrichten und werden den Sender sagen, dass diese hier nicht erwünscht sind. Schreibe "
+               "/commands, um meine Befehle zu erfahren. Es sind nicht viele :) Wenn du meinen "
+               "Source Code betrachten möchtest, sehr gerne: https://github.com/Poolitzer/DeletesVoiceMessagesBot. "
+               "Falls du Fragen, Anregungen oder Probleme hast, schreibe einfach meinen Master an, er kann auch "
+               "Deutsch: @Poolitzer "
+         }
+
+delete = {"en": "I'm sorry {}, but this group doesn't allow any voice message. This message will self destruct in {} "
+                "seconds.",
+          "de": "Hallo {}. Diese Gruppe erlaubt leider keine Sprachnachrichten, bitte sende keine. Diese Nachricht"
+                "wird sich in {} Sekunden selbst zerstören.",
+          }
+
+admin = {"en": "Sorry, only admins can change the language.",
+         "de": "Entschuldige, die SPrache dürfen nur Admins verändenr"
+         }
+
+setting = {"en": "Which language do you want from now on?",
+           "de": "Welche Sprache möchstest du einstellen?"
+           }
+
+starts = {"en": "You have to start me in private to be able to change the language",
+          "de": "Um die Sprache einstellen zu können musst du mich zuerst im privaten Chat starten."
+          }
+
+
+selected = {"en": "You selected *{}*.",
+            "de": "Du hast *{}* ausgewählt."
+            }
+
+private = {"en": "If you ever wonderd: No. I can't delete your (voice) messages in a private chat, thanks for trying "
+                 "though.",
+           "de": "Falls du dich jemals gefragt hast, ob Bots deine (Sprach-) Nachrichten im privaten Chat löschen "
+                 "können: Nein. Können wir nicht. Aber danke, dass du es noch mal probiert hast :D."
+           }
+
+fail = {"en": "I tried to delete this voice message, but I wasn't able to. Please check if you added me as an admin.",
+        "de": "Ich habe versucht, diese Sprachnachricht zu löschen, aber ich habe es nicht geschafft. Bitte überprüfe, "
+              "ob ich ein Admin bin"
+        }
+
+english = {"en": "English 🇬🇧🇺🇸",
+           "de": "Englisch 🇬🇧🇺🇸"
+           }
+
+german = {"en": "German 🇩🇪",
+          "de": "Deutsch 🇩🇪"
+          }
+
+joined = {"en": "Hello there. I will delete every voice message from now on. Please remember that I can only work as "
+                "an admin. If you need additional help, send /help. If you want to change the language, send /lang. "
+                "If you want to see stats for your group, send /stats.",
+          "de": "Hallo Gruppe. Ich werde von nun an alle Sprachnachrichten löschen. Bitte denk dran, dass ich nur als "
+                "Admin funktionieren kann. Wenn ihr Hilfe braucht, sendet einfach /help. Um die Sprache zu ändern, "
+                "sende /lang. Wenn ihr Statistiken zur Gruppe sehen wollt, sendet /stats."
+          }
+
+private_stats = {"en": "You tested if I'm able to delete voice message in private exactly {} times. i still can't",
+                 "de": "Du hast genau {} überprüft, ob ich Sprachnachrichten im privtaen CHat löschen kann. Kann ich "
+                       "nicht. "
+                 }
+
+group_stats = {"en": "I deleted {} voice messages in this group. Side note: {} tested {} times if I'm able to delete "
+                     "voice message in private. I'm not...",
+               "de": "Ich habe {} Sprachnachrichten in dieser Gruppe gelöscht. Nebenbei: {} hat {} mal überprüft, "
+                     "ob ich Sprachnachrichten im privaten Chat mit ihm löschen kann. Kann ich nicht... "
+               }
+
+commands_p = {"en": "*/start - starts me\n*/help - shows some general help\n*/lang - sets the anguage. In this private"
+                    " chat only for you, in a group for the group (admins only there\n"
+                    "/stats - shows how many voice messages I already deleted ",
+              "de": "*/start - startet mich\n*/help - sendet etwas generelle Hilfe\n*/lang - stellt die Sprache ein. "
+                    "In diesem privaten Chat für dich, in einer Gruppe für die Gruppe. Ist nur von Admins ausführbar."
+                    "\n*/stats - zeigt, wieviele Sprachnachrichten ich schon gelöscht habe. "
+              }
+commands_g = {"en": "/lang - sets the language. This is for admins only\n"
+                    "/stats - shows how many voice messages I deleted ",
+              "de": "**/lang - stellt die Sprache für die Gruppe ein. Ist nur von Admins ausführbar."
+                    "\n*/stats - zeigt, wieviele Sprachnachrichten ich schon gelöscht habe. "
+              }
